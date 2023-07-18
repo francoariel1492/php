cc388b764c4cda69bcadd6b3c2d9d6d8dec1d120
francoariel1492 <francoariel1492@gmail.com>
2023-07-18 16:02:27 +0300

    Variables

--- freecodecamp.php
+++ freecodecamp.php
@@ -16,6 +16,9 @@
 
     echo "There once was a man named $characterName<br>";
     echo "He was $characterAge years old<br>";
+
+    $characterName = "Mike";
+
     echo "He really liked the name $characterName<br>";
     echo "But didn't like being $characterAge<br>";
 
