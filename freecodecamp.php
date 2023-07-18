6688940814a5ecfb3bbbd215b676cafa58a8c26e
francoariel1492 <francoariel1492@gmail.com>
2023-07-18 15:47:52 +0300

    Starting with php

---
+++ freecodecamp.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    $characterName = "Franco";
+    $characterAge = 35;
+
+    echo "There once was a man named $characterName<br>";
+    echo "He was $characterAge years old<br>";
+    echo "He really liked the name $characterName<br>";
+    echo "But didn't like being $characterAge<br>";
+
+
+
+    ?>
+
+</body>
+
+</html>
