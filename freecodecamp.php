1f8dc3eaeee8a75e51267d66d2ef1b62c404d786
francoariel1492 <francoariel1492@gmail.com>
2023-07-23 19:42:21 +0200

    numbers

--- freecodecamp.php
+++ freecodecamp.php
@@ -10,7 +10,24 @@
 <body>
 
     <?php
-        echo 5 + 9;
+        $num = 10;
+        $num += 25;
+        $num -= 2;
+        echo $num;
+        echo "<br>";
+        echo abs(-250);
+        echo "<br>";
+        echo sqrt(144);
+        echo "<br>";
+        echo max(2,10);
+        echo "<br>";
+        echo min(2,10);
+        echo "<br>";
+        echo round(3.2);
+        echo "<br>";
+        echo ceil(3.3);
+        echo "<br>";
+        echo floor(3.3)
     ?>
 
 </body>
