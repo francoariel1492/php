c5c8af7ce7890a53bedadc9d76765a58edc84fbc
francoariel1492 <francoariel1492@gmail.com>
2023-07-24 21:41:38 +0200

    Building a better calculator

--- freecodecamp.php
+++ freecodecamp.php
@@ -9,17 +9,33 @@
 
 <body>
 
+    <form action="freecodecamp.php" method="post">
+        First num:<input type="number" step="0.1" name="num1">
+        <br>
+        OP: <input type="text" name="op">
+        <br>
+        Second num:<input type="number" step="0.1" name="num2">
+        <br>
+        <input type="submit">
+    </form>
+
     <?php
-        function getMax($num1,$num2, $num3){
-            if($num1 >= $num2 && $num1 >= $num3){
-                return $num1;
-            }elseif ($num2 >= $num1 && $num2 >= $num3){
-                return $num2;
-            }else{
-                return $num3;
-            }
+
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $op = $_POST["op"];
+
+        if($op == "+"){
+            echo $num1 + $num2;
+        }elseif ($op == "-"){
+            echo $num1 - $num2;
+        }elseif ($op == "/"){
+            echo $num1 / $num2;
+        }elseif ($op == "*"){
+            echo $num1 * $num2;
+        }else{
+            echo "Invalid operator";
         }
-        echo getMax(10,20,30)
     ?>
 
 </body>
